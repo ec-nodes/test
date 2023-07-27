7aa11781d829db5466614e5c74a6bb3d7831f549
ec-nodes <134415308+ec-nodes@users.noreply.github.com>
2023-07-27 22:37:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,8 +15,10 @@ async function fetchTransactions(nodeAddress) {
     }
   } catch (error) {
     console.log(error);
+    return; // Add this line to return undefined on error.
   }
 }
+
 function generateNewNodeAddressText(nodeAddress) {
   return window.innerWidth < window.innerHeight ? `${nodeAddress.substr(0, 5)}. . .${nodeAddress.substr(-4)}` : nodeAddress;
 }
@@ -99,5 +101,84 @@ document.addEventListener('DOMContentLoaded', async () => {
     });
   table.style.display = 'table';
 
-  // ... (rest of the code remains unchanged)
+  const addNodeBtn = document.getElementById('add-node');
+
+  addNodeBtn.addEventListener('click', async () => {
+    const nodeName = document.getElementById('node-name').value;
+    const nodeAddress = document.getElementById('node-address').value;
+
+    if (nodeName.trim() === '' || nodeAddress.trim() === '') {
+      alert('Please complete both fields!');
+      return;
+    }
+
+    if (existingAddresses.has(nodeAddress)) {
+      alert('This address already exists!');
+      return;
+    }
+
+    addNodeBtn.classList.add('clicked');
+    setTimeout(() => {
+      addNodeBtn.classList.remove('clicked');
+    }, 120);
+
+    const nodeData = await fetchTransactions({ nodeName, nodeAddress });
+    if (nodeData) {
+      addNodeToTable(nodeName, nodeAddress, nodeData.lastTransactionTime || 'Last Hour');
+      addNodeToDatabase(nodeName, nodeAddress);
+      document.getElementById('node-name').value = '';
+      document.getElementById('node-address').value = '';
+      existingAddresses.add(nodeAddress);
+    }
+  });
 });
+
+window.addEventListener('resize', () => {
+  const addresses = document.querySelectorAll('td:nth-child(2) a');
+  addresses.forEach((address) => {
+    address.textContent = generateNewNodeAddressText(address.textContent);
+  });
+});
+
+function downloadBackupJSON() {
+  const nodes = JSON.parse(localStorage.getItem('nodes')) || [];
+  const backupData = JSON.stringify(nodes, null, 2);
+  const blob = new Blob([backupData], { type: 'application/json' });
+  const url = URL.createObjectURL(blob);
+
+  const link = document.createElement('a');
+  link.href = url;
+  link.download = 'Nodes_Backup.json';
+  link.click();
+
+  URL.revokeObjectURL(url);
+}
+
+function restoreBackup() {
+  const input = document.createElement('input');
+  input.type = 'file';
+  input.accept = '.json';
+  input.addEventListener('change', async (event) => {
+    const file = event.target.files[0];
+    const contents = await file.text();
+    try {
+      const nodes = JSON.parse(contents);
+      if (Array.isArray(nodes)) {
+        localStorage.setItem('nodes', JSON.stringify(nodes));
+        location.reload();
+      } else {
+        throw new Error('Invalid backup file format.');
+      }
+    } catch (error) {
+      console.log('Error parsing backup file:', error);
+      alert('Error parsing backup file. Please make sure the file is in the correct format.');
+    }
+  });
+  input.click();
+}
+
+const downloadBackupBtn = document.getElementById('download-backup');
+downloadBackupBtn.addEventListener('click', downloadBackupJSON);
+
+const restoreBackupBtn = document.getElementById('restore-backup');
+restoreBackupBtn.addEventListener('click', restoreBackup);
