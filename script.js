6b189ee741c3d19f090db2521249f4cfec3664a2
ec-nodes <134415308+ec-nodes@users.noreply.github.com>
2023-08-08 12:38:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,17 @@ function addNodeToTable(nodeName, nodeAddress, transactionTime) {
   const newRow = table.insertRow();
   const newNodeAddressText = generateNewNodeAddressText(nodeAddress);
 
-  const transactionTimeText = typeof transactionTime === 'number' ? (transactionTime < 1 ? `${Math.round(transactionTime * 60)}m` : `${transactionTime}h`) : transactionTime;
+  let transactionTimeText = '';
+  if (typeof transactionTime === 'number') {
+    if (transactionTime < 1) {
+      const minutes = Math.round(transactionTime * 60);
+      transactionTimeText = `${minutes}m`;
+    } else {
+      transactionTimeText = `${transactionTime}h`;
+    }
+  } else {
+    transactionTimeText = transactionTime;
+  }
 
   newRow.innerHTML = `<td>${nodeName}</td><td><a href="https://blockexplorer.bloxberg.org/address/${nodeAddress}">${newNodeAddressText}</a></td><td>${transactionTimeText}</td><td><img src="https://i.ibb.co/xHbVTPk/delete-3.webp" alt="Delete" class="delete-logo"></td>`;
   const deleteLogo = newRow.querySelector('.delete-logo');
