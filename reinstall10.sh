bd87d030b494f9904d720092554497df1a760815
ec-nodes <134415308+ec-nodes@users.noreply.github.com>
2023-10-13 23:07:27 +0200

    Update reinstall10.sh

--- reinstall10.sh
+++ reinstall10.sh
@@ -82,12 +82,10 @@ RESET='\e[0m'
 
 while :
 do
-  clear
   echo -e "${GREEN}"
   echo -e "Run this command now or after reboot: cd ~/mvp-pox-node && sudo ./etny-node-installer.sh"
   echo -e "${RESET}"
   sleep 1
-  clear
   sleep 1
 done
 
