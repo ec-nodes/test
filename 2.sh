fcf5ae88cb52a88128f46f61657def560920220a
ec-nodes <134415308+ec-nodes@users.noreply.github.com>
2023-10-13 23:16:00 +0200

    Update and rename 1.sh to 2.sh

--- 2.sh
+++ 2.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 GREEN='\e[32m'
 RESET='\e[0m'
 
@@ -10,4 +8,11 @@ do
   echo -e "${RESET}"
   sleep 1
   sleep 1
+done &
+
+sec=30
+while [ $sec -ge 0 ]; do
+  echo -n "System Rebooting in [CTRL+C to cancel]: $sec seconds" && echo -ne "\033[0K\r" && let "sec=sec-1" && sleep 1
 done
+
+sudo reboot
