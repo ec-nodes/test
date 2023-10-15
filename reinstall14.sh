2b853fe342a472331c0eaf66429596847a1fd859
ec-nodes <134415308+ec-nodes@users.noreply.github.com>
2023-10-15 12:35:58 +0200

    Update and rename reinstall13.sh to reinstall14.sh

--- reinstall14.sh
+++ reinstall14.sh
@@ -12,7 +12,7 @@ sudo resize2fs /dev/mapper/ubuntu--vg-ubuntu--lv > /dev/null 2>&1
 
 sudo apt-mark unhold "qemu-system-common" "qemu-system-data" "qemu-system-x86" "qemu-utils"
 
-programs_to_uninstall=("qemu-*" "vagrant-*" "libvirt-*" "ansible" "sg3-utils-*" "libsgutils2-2" "acl" "attr" "bridge-utils" "bzip2" "checkpolicy" "cpp-11" "cpu-checker" "dconf-gsettings-backend:amd64" "dnsmasq-base" "ebtables" "exfatprogs" "f2fs-tools" "fontconfig-config" "fonts-lato" "guile-3.0-libs:amd64" "icu-devtools" "ipxe-qemu-256k-compat-efi-roms" "ipxe-qemu" "javascript-common" "jq" "libasound2-data" "libburn4:amd64" "libc6-dev:amd64" "libcacard0:amd64" "libdconf1:amd64" "libfdt1:amd64" "libgbm1:amd64" "libgstreamer-plugins-base1.0-0:amd64" "libibverbs1:amd64" "libjpeg8:amd64" "libjpeg-turbo8:amd64" "libnl-route-3-200:amd64" "libnss-libvirt:amd64" "libnss-mymachines:amd64" "liborc-0.4-0:amd64" "libphodav-2.0-0:amd64" "libpmem1:amd64" "libspice-server1:amd64" "liburing2:amd64" "libusbredirparser1:amd64" "libwayland-server0:amd64" "libyajl2:amd64" "mtools" "ovmf" "reiserfsprogs" "seabios" "systemd-container")
+programs_to_uninstall=("qemu-*" "vagrant-*" "libvirt-*" "ansible" "sg3-utils-*" "libsgutils2-2" "acl" "attr" "bridge-utils" "bzip2" "checkpolicy" "cpp-11" "cpu-checker" "dconf-gsettings-backend:amd64" "dnsmasq-base" "ebtables" "exfatprogs" "f2fs-tools" "fontconfig-config" "fonts-lato" "guile-3.0-libs:amd64" "icu-devtools" "libburn4:amd64" "libc6-dev:amd64" "libnss-libvirt:amd64" "libnss-mymachines:amd64" "libyajl2:amd64" "mtools" "ovmf" "reiserfsprogs" "systemd-container")
 
 total_programs="${#programs_to_uninstall[@]}"
 counter=0
