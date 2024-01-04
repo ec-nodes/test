02e39ee612bfad8f12c11ce921a837dd1f6ebe61
ec-nodes <134415308+ec-nodes@users.noreply.github.com>
2024-01-04 10:19:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -43,28 +43,20 @@ async function retryFetchTransactions(node, cell, progressInterval) {
     cell.textContent = 'Retrying';
     stopProgressAnimation(progressInterval);
 
-    await new Promise(resolve => setTimeout(resolve, 5000));
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    const retryResponse = await fetchTransactions(node);
 
-    try {
-        const retryResponse = await fetchTransactions(node);
-
-        if (retryResponse) {
-            cell.textContent = retryResponse.lastTransactionTime || 'Last Hour';
-            stopProgressAnimation(progressInterval);
-            if (typeof retryResponse.lastTransactionTime === 'number' && retryResponse.lastTransactionTime > 24) {
-                node.row.classList.add('red-text');
-            }
-        } else {
-            cell.textContent = 'Retry Failed';
-            stopProgressAnimation(progressInterval);
+    if (retryResponse) {
+        cell.textContent = retryResponse.lastTransactionTime || 'Last Hour';
+        stopProgressAnimation(progressInterval);
+        if (typeof retryResponse.lastTransactionTime === 'number' && retryResponse.lastTransactionTime > 24) {
+            node.row.classList.add('red-text');
         }
-    } catch (error) {
-        console.error(`Error during retry: ${error}`);
+    } else {
         cell.textContent = 'Retry Failed';
         stopProgressAnimation(progressInterval);
     }
 }
-
 
 function generateNewNodeAddressText(nodeAddress) {
     return window.innerWidth < window.innerHeight ? `${nodeAddress.substr(0, 5)}. . .${nodeAddress.substr(-4)}` : nodeAddress;
